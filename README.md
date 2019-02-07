e8e10c3fa76e139578b93c137b58826015d2100e
leejaeeun <leejaeeun@archive-asia.co.jp>
2019-02-07 15:02:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SwiftDrawSample
---
+++ SwiftDrawSample/DrawSampleView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawSampleView.swift
+//  SwiftDrawSample
+//
+//  Created by jaeeun on 2019/02/07.
+//  Copyright © 2019 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+class DrawSampleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
